6600489733af8bfa20c723a936f113c69471f353
heloi-neto <hey@heloineto.com>
2022-09-04 10:23:57 -0300

    fix: removed unnecessary hooks

--- src/components/pages/ListPage/ListPageLoading.tsx
+++ src/components/pages/ListPage/ListPageLoading.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from "react";
-import { ChatbotType } from "../../../@types/chatbots";
-import { getChatbots } from "../../../lib/api";
+import chatbots from "../../../lib/mocks/chatbots";
 import ActionButton from "../../elements/ActionButton";
 import Chatbot from "../../elements/Chatbot";
 import Add from "../../icons/Add";
 import ListPageHeader from "./ListPageHeader";
-import { View } from "./types";
 
 const ProfilePageLoading = () => {
-	const [view, setView] = useState<View>("grid");
-	const [chatbots, setChatbots] = useState<ChatbotType[] | null>(null);
-	const [untouchedChatbots, setUntouchedChatbots] = useState<
-		ChatbotType[] | null
-	>(null);
-	const [error, setError] = useState<unknown | null>(null);
-
-	useEffect(() => {
-		getChatbots()
-			.then((data) => {
-				setChatbots(data);
-				setUntouchedChatbots(data);
-			})
-			.catch((error) => {
-				setError(error);
-			});
-	}, []);
-
-	const value = {
-		view,
-		setView,
-		chatbots,
-		setChatbots,
-		untouchedChatbots,
-	};
-
 	return (
 		<div className="my-8">
 			<ListPageHeader />
